70094e108085c2df96387edcc588ccbacff35fb7
Emanuel <emanuel_vera@live.com.ar>
2022-08-03 00:58:16 -0300

    css in load

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import pokemon from "./pokemon.jpg";
-import stylesButton from "../globalStyles/buttonsStyle.module.css";
 import style from "./intro.module.css";
 
 export default function Intro() {
   return (
-    <div className={style.container}>
-      <img src={pokemon} alt="" />
-      <Link
-        to={`/home`}
-        className={`${stylesButton.buttonLink} ${style.button}`}
-      >
-        <button>Home</button>
+    <div className={style.intro}>
+      <div className={style.description}>
+        <h1>Proyecto Individual Pokemon</h1>
+        <p>
+          Demo donde se podra ver, buscar y crear <br />
+          pokemones usando la api externa pokeapi.co <br />
+          creador: Emanuel M Vera
+        </p>
+      </div>
+      <Link to={`/home`} className={style.button}>
+        <button>Comenzar</button>
       </Link>
     </div>
   );
